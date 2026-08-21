a91f1f4ec2ee0f3fe087f814691b867081553eec
Andrew Jackson <andrew@codechimp.org>
2026-08-21 14:10:11 +0100

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://andrew-codechimp.github.io',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
